8f9618d0df608403b6f05e31d14b1f5a2b2a7335
Nick-Home <20.de.octubre.i@gmail.com>
2021-08-18 15:59:04 -0400

    Categoria

--- controllers/CategoriaController.js
+++ controllers/CategoriaController.js
@@ -92,15 +92,15 @@ export default {
   desactivate: async (req, res, next) => {
     try {
       const reg = await models.Categoria.findByIdAndUpdate(
-        { _id: req.query._id },
+        { _id: req.body._id },
         { estado: 0 }
       );
       res.status(200).json(reg);
-    } catch (error) {
+    } catch (e) {
       res.status(500).send({
-        message: "Algo fall0...",
+        message: "Ocurrio un error",
       });
-      next(error);
-    }
-  }, //desactivar categorias
+      next(e);
+    } //desactivar categorias
+  },
 };
