02cbdf12e6e38e8ee7bbd47699f13cb33d0dd548
Nick-Home <20.de.octubre.i@gmail.com>
2021-08-17 20:22:33 -0400

    Arreglo desactivate

--- routes/ingreso.js
+++ routes/ingreso.js
@@ -8,6 +8,6 @@ router.post("/add", auth.verifyAlmacenero, ingresoController.add);
 router.get("/query", auth.verifyAlmacenero, ingresoController.query);
 router.get("/list", auth.verifyAlmacenero, ingresoController.list);
 router.put("/activate", auth.verifyAlmacenero, ingresoController.activate);
-router.put("/deactivate", auth.verifyAlmacenero, ingresoController.desactivate);
+router.put("/desactivate", auth.verifyAlmacenero, ingresoController.desactivate);
 
 export default router;
